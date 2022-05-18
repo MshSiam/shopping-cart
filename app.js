57af28a61c4c0e13e0f423ab271a7b05fc662d31
MshSiam <siyamkenway@gmail.com>
2022-05-18 20:02:29 +0600

    finished

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
+function updateProductNumber(product, price, isIncreasing) {
+  let productInput = document.getElementById(product + "-number")
+  let productNumber = productInput.value
+  let productN = parseInt(productNumber)
+  if (isIncreasing === true) {
+    productNumber = productN + 1
+  } else if (productNumber > 0 && isIncreasing === false) {
+    productNumber = productN - 1
+  }
+  productInput.value = productNumber
+  //   update case price
+  const caseTotal = document.getElementById(product + "-total")
+  caseTotal.innerText = productNumber * price
+  calculateTotal()
+}
+
+// calculate total--------------
+
+function getInputValue(product) {
+  const productInput = document.getElementById(product + "-number")
+  const productNumber = parseInt(productInput.value)
+  return productNumber
+}
+
+function calculateTotal() {
+  const phoneTotal = getInputValue("phone") * 1229
+  const caseTotal = getInputValue("case") * 59
+  const subTotal = phoneTotal + caseTotal
+  const tax = parseInt(subTotal * 0.1)
+  // update on html
+  document.getElementById("sub-total").innerText = subTotal
+  document.getElementById("tax-amount").innerText = tax
+  document.getElementById("total-price").innerText = subTotal + tax
+}
+
 document.getElementById("case-plus").addEventListener("click", function () {
-  //   console.log("hit")
-  let caseInput = document.getElementById("case-number")
-  let caseNumber = caseInput.value
-  let caseN = parseInt(caseNumber)
-  caseInput.value = caseN + 1
-  //   console.log(caseNumber)
+  updateProductNumber("case", 59, true)
+})
+
+document.getElementById("case-minus").addEventListener("click", function () {
+  updateProductNumber("case", 59, false)
+})
+
+// phone calculation
+document.getElementById("phone-plus").addEventListener("click", function () {
+  // console.log("clicked")
+  updateProductNumber("phone", 1229, true)
+})
+document.getElementById("phone-minus").addEventListener("click", function () {
+  // console.log("clicked")
+  updateProductNumber("phone", 1229, false)
 })
