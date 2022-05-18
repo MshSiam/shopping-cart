d46763a9f19d34c4719923f780bbb36ebf8593cd
MshSiam <siyamkenway@gmail.com>
2022-05-18 16:04:41 +0600

    plus

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ document.getElementById("case-plus").addEventListener("click", function () {
   //   console.log("hit")
   let caseInput = document.getElementById("case-number")
   let caseNumber = caseInput.value
+  let caseN = parseInt(caseNumber)
+  caseInput.value = caseN + 1
   //   console.log(caseNumber)
 })
